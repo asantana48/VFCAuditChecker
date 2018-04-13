541893736b5d5e972e810f994cc4e93cf8a51d28
Andres Santana <andres.santana@lascarelectronics.com>
2018-04-13 13:14:50 -0400

    Added test class and added another model.

---
+++ VFCAuditChecker/Models/Reading.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VFCAuditChecker.Models
+{
+    public class Reading
+    {
+        public String sessionGUID { get; set; }
+        public String datetime {get; set;}
+        public String readingType { get; set; }
+        public String channels { get; set; }
+    }
+}
---
+++ VFCAuditCheckerTests/CloudAPITests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VFCAuditChecker;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VFCAuditChecker.Tests
+{
+    [TestClass()]
+    public class CloudAPITests
+    {
+        /*
+         * Arrange
+         * Act
+         * Assert
+        */
+
+        public string getKey()
+        {
+            string PATH = "C:/Users/asantana/Desktop/VFCAuditChecker/VFCAuditChecker/resources/Credentials.txt";
+            string key = File.ReadAllText(PATH);
+            return key;
+        }
+
+        [TestMethod()]
+        public void GetStatus_Test()
+        {
+            // arrange
+            CloudAPI api = new CloudAPI(getKey());
+
+            // act
+            string status = api.GetStatus();
+
+            // assert
+            Assert.IsNotNull(status);
+        }
+
+        [TestMethod()]
+        public void Login_Valid()
+        {
+            // arrange
+            CloudAPI api = new CloudAPI(getKey());
+
+            // act
+            api.Login("andres.santana@lascarelectronics.com", "Lascarseddi12");
+            
+            // assert handled through exception
+        }
+
+        [TestMethod()]
+        public void Login_Invalid()
+        {
+            // Arrange
+            CloudAPI api = new CloudAPI(getKey());
+
+            // act
+            try
+            {
+                api.Login("fake@fake.com", "fake");
+            } catch (Exception e)
+            {
+                Assert.IsTrue(e.GetType() == typeof(CloudAPI.CloudException));
+            }
+
+            // assert 
+            //Assert.Fail("Expected CloudException");
+        }
+    }
+}
